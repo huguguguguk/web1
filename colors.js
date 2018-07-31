0f8033a1db25a7407c6369cb797da31e43ac566b
huguguguguk <41708238+huguguguguk@users.noreply.github.com>
2018-08-01 01:37:23 +0900

    Add files via upload

--- colors.js
+++ colors.js
@@ -1,19 +1,26 @@
-var Body = {
+	
+	
+		var Body = {
 			setBackGroundColor:function (color){
-				document.querySelector('body').style.backgroundColor =color;
+			//	document.querySelector('body').style.backgroundColor =color;
+			$('body').css('backgroundColor',color);
 			},
 			setColor:function (color){
-				document.querySelector('body').style.color=color;
+			//	document.querySelector('body').style.color=color;
+			$('body').css('color',color);
 			}
 		}
 		var Links ={
 			setColor:function (color){
-				var alist = document.querySelectorAll('a');
-				var i =0;
-				while(i<alist.length){
-					alist[i].style.color = color;
-					i = i+1;
-				}
+			$('a').css('color',color);
+			
+			//var alist = document.querySelectorAll('a');
+			//	var i =0;
+			//	while(i<alist.length){
+			//		alist[i].style.color = color;
+			//		i = i+1;
+			//	}
+			
 			}
 		}
 								
